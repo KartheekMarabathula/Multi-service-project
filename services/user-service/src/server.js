a1ff902a8ccb7b2f552cdcb6ed45e16f5db04cd7
kartheek <karthikmarabathula18@gmail.com>
2026-04-19 20:47:40 +0530

    changed data

--- services/user-service/src/server.js
+++ services/user-service/src/server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`User Service running on port ${PORT}`);
+  console.log(`User Service app running on port ${PORT}`);
 });
