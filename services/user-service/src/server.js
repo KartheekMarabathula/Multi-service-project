781add2f9d065bac8f30532dcccfd11c4fe0897f
kartheek <karthikmarabathula18@gmail.com>
2026-04-20 11:46:43 +0530

    edited 4

--- services/user-service/src/server.js
+++ services/user-service/src/server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`User Service app is running on port no ${PORT}`);
+  console.log(`User Service application is running on port no ${PORT}`);
 });
