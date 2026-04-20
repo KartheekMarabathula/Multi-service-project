3a819dab2e2db300250f577c7f075625bc6d65cf
kartheek <karthikmarabathula18@gmail.com>
2026-04-20 13:24:30 +0530

    udated image tag syntax

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.send("OK I am Healthy");
+  res.send("I am Healthy");
 });
 
 app.get("/users", async (req, res) => {
